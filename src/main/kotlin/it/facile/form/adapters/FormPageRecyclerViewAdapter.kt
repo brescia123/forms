6efb5ffe66f0927aa82810284e54a495ceafc677
Giacomo Bresciani <giacomo.bresciani@facile.it>
2016-08-05 17:38:52 +0200

    Fix Input field not hiding if having focus

--- src/main/kotlin/it/facile/form/adapters/FormPageRecyclerViewAdapter.kt
+++ src/main/kotlin/it/facile/form/adapters/FormPageRecyclerViewAdapter.kt
@@ -2,6 +2,7 @@ package it.facile.form.adapters
 
 import android.os.Handler
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import it.facile.form.R
 import it.facile.form.SectionedRecyclerViewAdapterK
 import it.facile.form.viewmodel.FieldValue
@@ -35,13 +36,16 @@ class FormPageRecyclerViewAdapter(sectionViewModels: List<SectionViewModel>,
         if (absolutePosition >= fieldsAdapter.itemCount) return // No field at given position
         if (viewModel.equals(fieldsAdapter.getViewModel(absolutePosition))) return // Same view model
 
+        val isHidingOrShowing = viewModel.hidden != fieldsAdapter.getViewModel(absolutePosition).hidden
+
         val sectionedPosition = positionToSectionedPosition(absolutePosition)
         fieldsAdapter.setFieldViewModel(absolutePosition, viewModel)
         setSection(sectionViewModel)
 
+        Log.d("FormPageAdapter", "updating field at absolute position $absolutePosition......")
         recyclerViews.map {
             val view = it.layoutManager.findViewByPosition(sectionedPosition)
-            if (!(view?.hasFocus() ?: true)) { // If the view has focus don't reload it
+            if (!(view?.hasFocus() ?: true) || isHidingOrShowing) { // If the view has focus don't reload it
                 if (it.isComputingLayout) { // Defer view update if RecyclerView is computing layout
                     deferredNotifyItemChanged(sectionedPosition)
                     deferredNotifyItemChanged(sectionViewModel.sectionedPosition)
@@ -49,6 +53,7 @@ class FormPageRecyclerViewAdapter(sectionViewModels: List<SectionViewModel>,
                     notifyItemChanged(sectionedPosition)
                     notifyItemChanged(sectionViewModel.sectionedPosition)
                 }
+                Log.d("FormPageAdapter", "notifying item at absolute position $absolutePosition changed. - \n $viewModel")
             }
         }
     }
