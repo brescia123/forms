60d2de9a781ca0086b4e7f7ad4006a87e05f0be9
Giacomo Bresciani <giacomo.bresciani@facile.it>
2016-08-10 12:03:54 +0200

    Make the form presenter unique (no pagepresenter)

---
+++ src/main/kotlin/it/facile/form/ui/FormPresenter.kt
@@ -0,0 +1,35 @@
+package it.facile.form.ui
+
+import it.facile.form.FormStorage
+import it.facile.form.model.FormModel
+
+class FormPresenter(val formModel: FormModel,
+                    val storage: FormStorage) : Presenter<FormView>() {
+
+    companion object {
+        val TAG: String = "FormPresenter"
+    }
+
+    override fun attach(view: FormView) {
+        super.attach(view)
+        view.init(formModel.pages.map { it.buildPageViewModel(storage) })  // I
+        view.observeValueChanges().retry().subscribe(
+                { formModel.notifyValueChanged(it.first, it.second) },
+                { view.logE(TAG, it.message) })
+        formModel.observeChanges().subscribe(
+                {
+                    val (fieldPath, fieldViewModel) = it
+                    val absolutePosition = formModel.pages[fieldPath.pageIndex].buildAbsoluteFieldPositionFromFieldPath(fieldPath)
+                    if (absolutePosition != null) {
+                        val sectionViewModel = formModel.pages[fieldPath.pageIndex].sections[fieldPath.sectionIndex].buildSectionViewModel(storage)
+                        view.updateField(fieldPath.pageIndex, absolutePosition, fieldViewModel, sectionViewModel)
+                    }
+                },
+                { view.logE(TAG, it.message) }
+        )
+    }
+
+    override fun detach() {
+        super.detach()
+    }
+}
