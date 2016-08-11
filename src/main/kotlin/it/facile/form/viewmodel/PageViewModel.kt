f3dccad517321b793c90446eacca0ce3f3ef9b2d
Giacomo Bresciani <giacomo.bresciani@facile.it>
2016-08-11 11:32:16 +0200

    Remove Parcelable

--- src/main/kotlin/it/facile/form/viewmodel/PageViewModel.kt
+++ src/main/kotlin/it/facile/form/viewmodel/PageViewModel.kt
@@ -1,28 +1,8 @@
 package it.facile.form.viewmodel
 
-import android.os.Parcel
-import android.os.Parcelable
-import java.util.*
-
 data class PageViewModel(val title: String,
-                         val sections: List<SectionViewModel>) : ViewModel, Parcelable {
+                         val sections: List<SectionViewModel>) : ViewModel {
     override fun isHidden(): Boolean {
         return sections.filter { !it.isHidden() }.size == 0
-    }
-
-    constructor(source: Parcel): this(source.readString(), ArrayList<SectionViewModel>().apply{ source.readList(this, SectionViewModel::class.java.classLoader) })
-
-    override fun describeContents() = 0
-
-    override fun writeToParcel(dest: Parcel?, flags: Int) {
-        dest?.writeString(title)
-        dest?.writeList(sections)
-    }
-
-    companion object {
-        @JvmField val CREATOR: Parcelable.Creator<PageViewModel> = object : Parcelable.Creator<PageViewModel> {
-            override fun createFromParcel(source: Parcel): PageViewModel = PageViewModel(source)
-            override fun newArray(size: Int): Array<PageViewModel?> = arrayOfNulls(size)
-        }
     }
 }
