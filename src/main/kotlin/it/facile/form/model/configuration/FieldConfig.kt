c8c6f7df08264d7aeee2268507951f9bc709ccd2
Giacomo Bresciani <giacomo.bresciani@facile.it>
2016-09-05 12:41:13 +0200

    Add validation interfaces

--- src/main/kotlin/it/facile/form/model/configuration/FieldConfig.kt
+++ src/main/kotlin/it/facile/form/model/configuration/FieldConfig.kt
@@ -5,7 +5,15 @@ import it.facile.form.viewmodel.FieldViewModel
 import it.facile.form.viewmodel.FieldViewModelStyle
 import rx.Single
 
+/* ---------- Configurations ---------- */
+
 abstract class FieldConfig(val label: String) : ViewModelGenerator, ViewModelStyleGenerator {}
+
+interface DeferredConfig {
+    fun observe(): Single<Unit>
+}
+
+/* ---------- View Models ---------- */
 
 interface ViewModelGenerator {
     fun getViewModel(value: FieldValue, hidden: Boolean): FieldViewModel
@@ -15,6 +23,20 @@ interface ViewModelStyleGenerator {
     fun getViewModelStyle(value: FieldValue): FieldViewModelStyle
 }
 
-interface DeferredConfig {
-    fun observe(): Single<Unit>
+/* ---------- Rules ---------- */
+
+interface FieldRulesValidator {
+    /** Return an error message if the value doesn't satisfy at least one rule, null otherwise */
+    fun isValid(rules: List<FieldRule>, value: FieldValue): String? {
+        rules.map {
+            val verify = it.verify(value)
+            if (!verify.first) return verify.second
+        }
+        return null
+    }
+}
+
+interface FieldRule {
+    /** Return a if the value satisfies the rule and the error message to use if it doesn't */
+    fun verify(value: FieldValue): Pair<Boolean, String>
 }
