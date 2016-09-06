c00370ba171d4f20de5715eb0dbdceabea166b80
Giacomo Bresciani <giacomo.bresciani@facile.it>
2016-09-06 15:35:46 +0200

    Fix multiple subscriptions bug

--- src/main/kotlin/it/facile/form/ui/FormPresenter.kt
+++ src/main/kotlin/it/facile/form/ui/FormPresenter.kt
@@ -2,35 +2,42 @@ package it.facile.form.ui
 
 import it.facile.form.FormStorage
 import it.facile.form.model.FormModel
+import rx.subscriptions.CompositeSubscription
 
 class FormPresenter(val formModel: FormModel,
                     val storage: FormStorage) : Presenter<FormView>() {
 
     companion object {
-        val TAG: String = "FormPresenter"
+        val TAG = "FormPresenter"
     }
+
+    private val subscriptions by lazy { CompositeSubscription() }
 
     override fun attach(view: FormView) {
         super.attach(view)
         view.init(formModel.pages.map { it.buildPageViewModel(storage) })  // Init view with viewModels
 
         // Observe values from view (FieldPathValue)
-        view.observeValueChanges().retry().subscribe(
-                { formModel.notifyValueChanged(it.path, it.value) },
-                { view.logE(TAG, it.message) })
+        subscriptions.add(
+                view.observeValueChanges().retry().subscribe(
+                        { formModel.notifyValueChanged(it.path, it.value) },
+                        { view.logE(TAG, it.message) }))
+
 
         // Observe paths from model (FieldPath)
-        formModel.observeChanges().subscribe(
-                {
-                    val fieldViewModel = formModel.getField(it).buildFieldViewModel(storage)
-                    val sectionViewModel = formModel.getSection(it).buildSectionViewModel(storage)
-                    view.updateField(it, fieldViewModel, sectionViewModel)
-                },
-                { view.logE(TAG, it.message) }
-        )
+        subscriptions.add(
+                formModel.observeChanges().subscribe(
+                        {
+                            val fieldViewModel = formModel.getField(it).buildFieldViewModel(storage)
+                            val sectionViewModel = formModel.getSection(it).buildSectionViewModel(storage)
+                            view.updateField(it, fieldViewModel, sectionViewModel)
+                        },
+                        { view.logE(TAG, it.message) }))
+
     }
 
     override fun detach() {
         super.detach()
+        subscriptions.clear()
     }
 }
