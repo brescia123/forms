c8c8d971d16bfc702f138ce51088dd3e0a6ae409
Giacomo Bresciani <giacomo.bresciani@facile.it>
2016-09-08 10:33:00 +0200

    Make values not null

--- src/main/kotlin/it/facile/form/viewmodel/FieldValue.kt
+++ src/main/kotlin/it/facile/form/viewmodel/FieldValue.kt
@@ -6,14 +6,14 @@ sealed class FieldValue {
     object Missing : FieldValue()
     class Text(val text: String = "") : FieldValue()
     class Bool(val bool: Boolean = false) : FieldValue()
-    class DateValue(val date: Date?): FieldValue()
-    class Object(val value: DescribableWithKey?) : FieldValue()
+    class DateValue(val date: Date): FieldValue()
+    class Object(val value: DescribableWithKey) : FieldValue()
 
     override fun toString(): String = when (this) {
         is Text -> text.toString()
         is Bool -> bool.toString()
-        is Object -> value?.describe() ?: NO_VALUE
-        is DateValue -> date?.toString() ?: NO_VALUE
+        is Object -> value.describe()
+        is DateValue -> date.toString()
         is Missing -> "Missing value"
     }
 
@@ -23,12 +23,8 @@ sealed class FieldValue {
             is Missing -> true
             is Text -> other is Text && text.equals(other.text)
             is Bool -> other is Bool && bool == other.bool
-            is DateValue -> other is Object && date?.equals(other.value) ?: other.value == null
-            is Object -> other is Object && value?.equals(other.value) ?: other.value == null
+            is DateValue -> other is Object && date.equals(other.value)
+            is Object -> other is Object && value.equals(other.value)
         }
-    }
-
-    companion object {
-        private val NO_VALUE: String = "No selected value"
     }
 }
