bcc8212b67041a211a22aa59a0019cd0a1841718
Giacomo Bresciani <giacomo.bresciani@facile.it>
2016-09-21 19:00:54 +0200

    Add hash code to FieldValue

--- src/main/kotlin/it/facile/form/viewmodel/FieldValue.kt
+++ src/main/kotlin/it/facile/form/viewmodel/FieldValue.kt
@@ -6,7 +6,7 @@ sealed class FieldValue {
     object Missing : FieldValue()
     class Text(val text: String = "") : FieldValue()
     class Bool(val bool: Boolean = false) : FieldValue()
-    class DateValue(val date: Date): FieldValue()
+    class DateValue(val date: Date) : FieldValue()
     class Object(val value: DescribableWithKey) : FieldValue()
 
     override fun toString(): String = when (this) {
@@ -19,12 +19,20 @@ sealed class FieldValue {
 
     override fun equals(other: Any?): Boolean {
         return if (other == null) false
-        else when(this) {
+        else when (this) {
             is Missing -> true
             is Text -> other is Text && text.equals(other.text)
             is Bool -> other is Bool && bool == other.bool
             is DateValue -> other is Object && date.equals(other.value)
             is Object -> other is Object && value.equals(other.value)
         }
+    }
+
+    override fun hashCode(): Int = when (this) {
+        is Missing -> 31
+        is Text -> text.hashCode()
+        is Bool -> bool.hashCode() * 31
+        is DateValue -> date.hashCode() * 31
+        is Object -> value.hashCode()
     }
 }
