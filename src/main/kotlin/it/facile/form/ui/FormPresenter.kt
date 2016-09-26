31b71e0ab9929afc863aea2c817402ba9904ee53
Giacomo Bresciani <giacomo.bresciani@facile.it>
2016-09-26 17:52:01 +0200

    Presenter fix

--- src/main/kotlin/it/facile/form/ui/FormPresenter.kt
+++ src/main/kotlin/it/facile/form/ui/FormPresenter.kt
@@ -2,17 +2,15 @@ package it.facile.form.ui
 
 import it.facile.form.logE
 import it.facile.form.model.models.FormModel
-import it.facile.form.storage.FormStorage
 import rx.subscriptions.CompositeSubscription
 
-class FormPresenter(val formModel: FormModel,
-                    val storage: FormStorage) : Presenter<FormView>() {
+class FormPresenter(val formModel: FormModel) : Presenter<FormView>() {
 
     private val subscriptions by lazy { CompositeSubscription() }
 
     override fun attach(view: FormView) {
         super.attach(view)
-        view.init(formModel.pages.map { it.buildPageViewModel(storage) })  // Init view with viewModels
+        view.init(formModel.pages.map { it.buildPageViewModel(formModel.storage) })  // Init view with viewModels
 
         // Observe values from view (FieldPathValue)
         subscriptions.add(
@@ -25,8 +23,8 @@ class FormPresenter(val formModel: FormModel,
         subscriptions.add(
                 formModel.observeChanges().subscribe(
                         {
-                            val fieldViewModel = formModel.getField(it).buildFieldViewModel(storage)
-                            val sectionViewModel = formModel.getSection(it).buildSectionViewModel(storage)
+                            val fieldViewModel = formModel.getField(it).buildFieldViewModel(formModel.storage)
+                            val sectionViewModel = formModel.getSection(it).buildSectionViewModel(formModel.storage)
                             view.updateField(it, fieldViewModel, sectionViewModel)
                         },
                         { logE(it.message) }))
