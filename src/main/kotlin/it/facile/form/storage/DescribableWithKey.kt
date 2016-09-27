3ca92b2a61f1b84013972ec0b88b13085ffe85d0
Giacomo Bresciani <giacomo.bresciani@facile.it>
2016-09-27 15:18:22 +0200

    Make DescribableWithKey more flexible and easy to instantiate

--- src/main/kotlin/it/facile/form/storage/DescribableWithKey.kt
+++ src/main/kotlin/it/facile/form/storage/DescribableWithKey.kt
@@ -2,5 +2,15 @@ package it.facile.form.storage
 
 interface DescribableWithKey {
     val key: Int
-    fun describe(): String
+    val text: String
+    operator fun component1(): Int = key
+    operator fun component2() = text
+}
+
+infix fun Int.keyTo(that: String): DescribableWithKey {
+    val keyInt = this
+    return object : DescribableWithKey {
+        override val key: Int = keyInt
+        override val text: String = that
+    }
 }
