8ac95b6c428b0c1826683a7c77ea5e0f5b62e947
Giacomo Bresciani <giacomo.bresciani@facile.it>
2016-09-27 15:19:01 +0200

    Add default message to rules and the possibility to giva them a custom one

--- src/main/kotlin/it/facile/form/FieldRules.kt
+++ src/main/kotlin/it/facile/form/FieldRules.kt
@@ -5,9 +5,7 @@ import it.facile.form.model.KeyReader
 import it.facile.form.storage.FieldValue
 import it.facile.form.storage.FieldValue.Text
 
-class NotMissing() : FieldRule() {
-    override val errorMessage = "Field should not be empty"
-
+class NotMissing(override val errorMessage: String = "Field should not be empty") : FieldRule() {
     override fun verify(value: FieldValue) = when (value) {
         is FieldValue.Missing -> false
         is Text -> value.text.length > 0
@@ -17,28 +15,25 @@ class NotMissing() : FieldRule() {
     override fun observedKeys() = emptyList<KeyReader>()
 }
 
-class IsEmail() : FieldRule() {
+class IsEmail(override val errorMessage: String = "Field should be a valid email") : FieldRule() {
     val emailRegex = Regex("[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}\\@" +
             "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
             "(\\.[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25})+")
-    override val errorMessage = "Field should be a valid email"
 
     override fun verify(value: FieldValue) = value.asText()?.text?.matches(emailRegex) ?: false
     override fun observedKeys() = emptyList<KeyReader>()
 }
 
-class IsCellularPhone() : FieldRule() {
+class IsCellularPhone(override val errorMessage: String = "Field should be a valid phone number") : FieldRule() {
     val phoneETACSRegex = Regex("^3[0-9]{8}")
     val phoneGSMRegex = Regex("^3[0-9]{9}")
-    override val errorMessage = "Field should be a valid phone number"
     override fun verify(value: FieldValue) =
             value.asText()?.text?.matchesAtLeastOne(phoneETACSRegex, phoneGSMRegex) ?: false
 
     override fun observedKeys() = emptyList<KeyReader>()
 }
 
-class ShouldBeTrue() : FieldRule() {
-    override val errorMessage = "Field should be true"
+class ShouldBeTrue(override val errorMessage: String = "Field should be true") : FieldRule() {
     override fun verify(value: FieldValue) = value.asBool()?.bool ?: false
     override fun observedKeys() = emptyList<KeyReader>()
 }
