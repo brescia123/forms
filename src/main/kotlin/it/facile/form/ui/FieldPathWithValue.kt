5ca266967df7eb742a14a26b78bb4959045f52c5
Giacomo Bresciani <giacomo.bresciani@facile.it>
2016-10-04 12:45:43 +0200

    Add infix operator "pathTo"

--- src/main/kotlin/it/facile/form/ui/FieldPathWithValue.kt
+++ src/main/kotlin/it/facile/form/ui/FieldPathWithValue.kt
@@ -4,3 +4,5 @@ import it.facile.form.storage.FieldValue
 import it.facile.form.ui.viewmodel.FieldPath
 
 data class FieldPathWithValue(val path: FieldPath, val value: FieldValue)
+
+infix fun FieldPath.pathTo(value: FieldValue) = FieldPathWithValue(this, value)
