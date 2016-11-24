ad65fccba30ae318e0c267ef9e39bf2a59d63b85
Giacomo Bresciani <brescia123@gmail.com>
2016-11-24 13:12:53 +0100

    Improve form DSL

--- src/main/kotlin/it/facile/form/model/ModelBuilder.kt
+++ src/main/kotlin/it/facile/form/model/ModelBuilder.kt
@@ -8,10 +8,8 @@ import it.facile.form.model.models.FieldModel
 import it.facile.form.model.models.FormModel
 import it.facile.form.model.models.PageModel
 import it.facile.form.model.models.SectionModel
-import it.facile.form.model.serialization.FieldSerialization
 import it.facile.form.model.serialization.FieldSerializationApi
-import it.facile.form.model.serialization.FieldSerializationRule
-import it.facile.form.model.serialization.FieldSerializationStrategy
+import it.facile.form.model.serialization.NEVER_SERIALIZE
 import it.facile.form.storage.FieldPossibleValues
 import it.facile.form.storage.FieldValue
 import it.facile.form.storage.FormStorage
@@ -43,107 +41,152 @@ fun PageModel.section(title: String, init: SectionModel.() -> Unit): SectionMode
     return section
 }
 
-/** Type-safe builder method to add a field */
-fun SectionModel.field(key: String,
-          serialization: FieldSerialization = FieldSerialization(FieldSerializationRule.NEVER, FieldSerializationStrategy.None),
-          config: FieldConfig): FieldModel {
-    val field = FieldModel(key, serialization, config)
-    fields.add(field)
-    return field
+interface FieldBuilder {
+    fun build(): FieldModel
 }
 
-fun SectionModel.fieldd(key: String, init: FieldModelBuilder.() -> Unit): FieldModel {
-    val fieldModel = FieldModelBuilder(key).apply(init).build()
+
+/* ---------- EMPTY ---------- */
+
+fun SectionModel.empty(label: String): FieldModel {
+    val fieldModel = FieldModel("", NEVER_SERIALIZE, FieldConfigEmpty(label))
     fields.add(fieldModel)
     return fieldModel
 }
 
+/* ---------- CHECKBOX ---------- */
 
-class FieldModelBuilder(val key: String) {
-    var serialization: FieldSerializationApi = FieldSerialization(FieldSerializationRule.NEVER, FieldSerializationStrategy.None)
-    var configuration: FieldConfigApi = FieldConfigEmpty("")
-    fun build() = FieldModel(key, serialization, configuration)
-}
-
-class FieldConfigCheckboxBuilder(private val label: String) {
+class FieldCheckboxBuilder(private val key: String) : FieldBuilder {
+    var label: String = ""
     var boolToStringConverter: ((Boolean) -> String) = { "" }
     var rules: (FormStorage) -> List<FieldRule> = { emptyList() }
-    fun build() = FieldConfigBool(label, CHECKBOX, boolToStringConverter, rules)
+    var serialization: FieldSerializationApi = NEVER_SERIALIZE
+    override fun build() =
+            FieldModel(key, serialization, FieldConfigBool(label, CHECKBOX, boolToStringConverter, rules))
 }
 
-class FieldConfigToggleBuilder(private val label: String) {
+fun SectionModel.checkbox(key: String, init: FieldCheckboxBuilder.() -> Unit): FieldModel {
+    val fieldModel = FieldCheckboxBuilder(key).apply(init).build()
+    fields.add(fieldModel)
+    return fieldModel
+}
+
+/* ---------- TOGGLE ---------- */
+
+class FieldToggleBuilder(private val key: String) : FieldBuilder {
+    var label: String = ""
     var boolToStringConverter: ((Boolean) -> String) = { "" }
     var rules: (FormStorage) -> List<FieldRule> = { emptyList() }
-    fun build() = FieldConfigBool(label, TOGGLE, boolToStringConverter, rules)
+    var serialization: FieldSerializationApi = NEVER_SERIALIZE
+    override fun build() =
+            FieldModel(key, serialization, FieldConfigBool(label, TOGGLE, boolToStringConverter, rules))
 }
 
-class FieldConfigPickerBuilder(private val label: String) {
+fun SectionModel.toggle(key: String, init: FieldToggleBuilder.() -> Unit): FieldModel {
+    val fieldModel = FieldToggleBuilder(key).apply(init).build()
+    fields.add(fieldModel)
+    return fieldModel
+}
+
+/* ---------- CUSTOM BEHAVIOUR ---------- */
+
+class FieldCustomBehaviourBuilder(private val key: String, val behaviourId: String) : FieldBuilder {
+    var label: String = ""
+    var serialization: FieldSerializationApi = NEVER_SERIALIZE
+    override fun build() = FieldModel(key, serialization, FieldConfigCustomBehaviour(label, behaviourId))
+}
+
+fun SectionModel.customBehaviour(key: String, customBehaviourId: String, init: FieldCustomBehaviourBuilder.() -> Unit): FieldModel {
+    val fieldModel = FieldCustomBehaviourBuilder(key, customBehaviourId).apply(init).build()
+    fields.add(fieldModel)
+    return fieldModel
+}
+
+/* ---------- DEFERRED CONFIG ---------- */
+
+class FieldDeferredBuilder(private val key: String) : FieldBuilder {
+    var label: String = ""
+    var deferredConfig: Single<FieldConfig> = FieldConfigEmpty(label).toSingle()
+    var errorMessage: String = "Loading error"
+    var serialization: FieldSerializationApi = NEVER_SERIALIZE
+    override fun build() =
+            FieldModel(key, serialization, FieldConfigDeferred(label, deferredConfig, errorMessage))
+}
+
+fun SectionModel.deferred(key: String, init: FieldDeferredBuilder.() -> Unit): FieldModel {
+    val fieldModel = FieldDeferredBuilder(key).apply(init).build()
+    fields.add(fieldModel)
+    return fieldModel
+}
+
+/* ---------- INPUT TEXT ---------- */
+
+class FieldInputTextBuilder(private val key: String) : FieldBuilder {
+    var label: String = ""
+    var rules: (FormStorage) -> List<FieldRule> = { emptyList() }
+    var inputTextType: InputTextType = InputTextType.TEXT
+    var serialization: FieldSerializationApi = NEVER_SERIALIZE
+    override fun build() =
+            FieldModel(key, serialization, FieldConfigInputText(label, rules, inputTextType))
+}
+
+fun SectionModel.input(key: String, init: FieldInputTextBuilder.() -> Unit): FieldModel {
+    val fieldModel = FieldInputTextBuilder(key).apply(init).build()
+    fields.add(fieldModel)
+    return fieldModel
+}
+
+/* ---------- PICKER ---------- */
+
+class FieldPickerBuilder(private val key: String) : FieldBuilder {
+    var label: String = ""
     var possibleValues: FieldPossibleValues = FieldPossibleValues.Available(emptyList())
     var placeHolder: String = "Select a value"
     var errorMessage: String = "Loading error"
     var rules: (FormStorage) -> List<FieldRule> = { emptyList() }
-    fun build() = FieldConfigPicker(label, possibleValues, placeHolder, errorMessage, rules)
+    var serialization: FieldSerializationApi = NEVER_SERIALIZE
+    override fun build() =
+            FieldModel(key, serialization, FieldConfigPicker(label, possibleValues, placeHolder, errorMessage, rules))
 }
 
-class FieldConfigDeferredBuilder(private val label: String) {
-    var deferredConfig: Single<FieldConfig> = FieldConfigEmpty(label).toSingle()
-    var errorMessage: String = "Loading error"
-    fun build() = FieldConfigDeferred(label, deferredConfig, errorMessage)
+fun SectionModel.picker(key: String, init: FieldPickerBuilder.() -> Unit): FieldModel {
+    val fieldModel = FieldPickerBuilder(key).apply(init).build()
+    fields.add(fieldModel)
+    return fieldModel
 }
 
-class FieldConfigInputTextBuilder(private val label: String) {
-    var rules: (FormStorage) -> List<FieldRule> = { emptyList() }
-    var inputTextType: InputTextType = InputTextType.TEXT
-    fun build() = FieldConfigInputText(label, rules, inputTextType)
-}
+/* ---------- PICKER CUSTOM ---------- */
 
-class FieldConfigPickerCustomBuilder(private val label: String, val id: String) {
+class FieldPickerCustomBuilder(private val key: String, val customPickerId: String) : FieldBuilder {
+    var label: String = ""
     var placeHolder: String = "Select a value"
     var rules: (FormStorage) -> List<FieldRule> = { emptyList() }
-    fun build() = FieldConfigPickerCustom(label, id, placeHolder, rules)
+    var serialization: FieldSerializationApi = NEVER_SERIALIZE
+    override fun build() = FieldModel(key, serialization, FieldConfigPickerCustom(label, customPickerId, placeHolder, rules))
 }
 
-class FieldConfigPickerDateBuilder(private val label: String) {
+fun SectionModel.pickerCustom(key: String, customPickerId: String, init: FieldPickerCustomBuilder.() -> Unit): FieldModel {
+    val fieldModel = FieldPickerCustomBuilder(key, customPickerId).apply(init).build()
+    fields.add(fieldModel)
+    return fieldModel
+}
+
+/* ---------- PICKER DATE ---------- */
+
+class FieldConfigPickerDateBuilder(private val key: String) : FieldBuilder {
+    var label: String = ""
     var minDate: Date = Dates.create(1900, 0, 1)
     var maxDate: Date = Dates.create(2100, 11, 31)
     var dateFormatter: DateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM)
     var placeHolder: String = "Select a date"
     var rules: (FormStorage) -> List<FieldRule> = { emptyList() }
-    fun build() = FieldConfigPickerDate(label, minDate, maxDate, dateFormatter, placeHolder, rules)
+    var serialization: FieldSerializationApi = NEVER_SERIALIZE
+    override fun build() =
+            FieldModel(key, serialization, FieldConfigPickerDate(label, minDate, maxDate, dateFormatter, placeHolder, rules))
 }
 
-
-/* Configurations */
-
-fun FieldModelBuilder.empty(label: String) {
-    configuration = FieldConfigEmpty(label)
-}
-fun FieldModelBuilder.checkbox(label: String, init: FieldConfigCheckboxBuilder.() -> Unit) {
-    configuration = FieldConfigCheckboxBuilder(label).apply(init).build()
-}
-fun FieldModelBuilder.toggle(label: String, init: FieldConfigToggleBuilder.() -> Unit) {
-    configuration = FieldConfigToggleBuilder(label).apply(init).build()
-}
-fun FieldModelBuilder.customBehaviour(label: String, customBehaviourId: String) {
-    configuration = FieldConfigCustomBehaviour(label, customBehaviourId)
-}
-fun FieldModelBuilder.deferred(label: String, init: FieldConfigDeferredBuilder.() -> Unit) {
-    configuration = FieldConfigDeferredBuilder(label).apply(init).build()
-}
-fun FieldModelBuilder.input(label: String, init: FieldConfigInputTextBuilder.() -> Unit) {
-    configuration = FieldConfigInputTextBuilder(label).apply(init).build()
-}
-fun FieldModelBuilder.picker(label: String, init: FieldConfigPickerBuilder.() -> Unit) {
-    configuration = FieldConfigPickerBuilder(label).apply(init).build()
-}
-fun FieldModelBuilder.customPicker(label: String, id: String, init: FieldConfigPickerCustomBuilder.() -> Unit) {
-    configuration = FieldConfigPickerCustomBuilder(label, id).apply(init).build()
-}
-fun FieldModelBuilder.datePicker(label: String, init: FieldConfigPickerDateBuilder.() -> Unit) {
-    configuration = FieldConfigPickerDateBuilder(label).apply(init).build()
-}
-
-/* Serialization */
-fun FieldModelBuilder.serialization(init: () -> FieldSerializationApi) {
-    serialization = init()
+fun SectionModel.pickerDate(key: String, init: FieldConfigPickerDateBuilder.() -> Unit): FieldModel {
+    val fieldModel = FieldConfigPickerDateBuilder(key).apply(init).build()
+    fields.add(fieldModel)
+    return fieldModel
 }
