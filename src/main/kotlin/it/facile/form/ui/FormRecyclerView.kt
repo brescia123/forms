3504dd52992463d404559be0b53e847b4d1fc465
Giacomo Bresciani <brescia123@gmail.com>
2016-12-13 18:41:44 +0100

    Move MVP logic to FormRecyclerView

--- src/main/kotlin/it/facile/form/ui/FormRecyclerView.kt
+++ src/main/kotlin/it/facile/form/ui/FormRecyclerView.kt
@@ -1,20 +1,66 @@
 package it.facile.form.ui
 
 import android.content.Context
+import android.os.Handler
+import android.support.design.widget.Snackbar
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.util.AttributeSet
+import it.facile.form.R
+import it.facile.form.addTo
+import it.facile.form.logE
 import it.facile.form.logW
+import it.facile.form.model.models.FormModel
+import it.facile.form.model.models.FormModel.FormState.*
+import it.facile.form.storage.FieldValue
+import it.facile.form.storage.FormStorage
+import it.facile.form.ui.adapters.SectionsAdapter
 import it.facile.form.ui.utils.FormDefaultItemAnimator
+import it.facile.form.ui.viewmodel.FieldPath
+import it.facile.form.ui.viewmodel.FieldPathSection
+import it.facile.form.ui.viewmodel.SectionViewModel
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
+import rx.subscriptions.CompositeSubscription
 
 class FormRecyclerView(context: Context, attrs: AttributeSet? = null) : RecyclerView(context, attrs) {
     constructor(context: Context, attrs: AttributeSet?, defStyle: Int) : this(context, attrs) {
         RecyclerView(context, attrs, defStyle)
     }
 
+    private val subscriptions by lazy { CompositeSubscription() }
+    private val formLoadingErrorSnackbar: Snackbar by lazy {
+        Snackbar.make(this, errorMessage, Snackbar.LENGTH_INDEFINITE)
+                .setAction(retryMessage, {
+                    // Defer retry to allow the snackbar to fade away
+                    Handler().postDelayed({ formModel.loadDynamicValues() }, 400)
+                })
+    }
+
+    var errorMessage = "There was an error when trying to load the form"
+    var retryMessage = "Retry"
+    var errorVisualization = true
+    var pageIndex = 0
+    var formModel: FormModel = FormModel(FormStorage.empty(), mutableListOf(), mutableListOf())
+        set(value) {
+            field = value
+            initModel()
+        }
+
     init {
         itemAnimator = FormDefaultItemAnimator()
         layoutManager = LinearLayoutManager(context)
+        val a = context.theme.obtainStyledAttributes(
+                attrs,
+                R.styleable.FormRecyclerView,
+                0, 0)
+        try {
+            if (a.hasValue(R.styleable.FormRecyclerView_errorMessage)) errorMessage = a.getString(R.styleable.FormRecyclerView_errorMessage)
+            if (a.hasValue(R.styleable.FormRecyclerView_retryMessage)) retryMessage = a.getString(R.styleable.FormRecyclerView_retryMessage)
+            errorVisualization = a.getBoolean(R.styleable.FormRecyclerView_errorVisualization, true)
+        } finally {
+            a.recycle()
+        }
     }
 
     override fun setItemAnimator(animator: ItemAnimator?) {
@@ -23,4 +69,79 @@ class FormRecyclerView(context: Context, attrs: AttributeSet? = null) : Recycler
                     "with input text fields.")
         super.setItemAnimator(animator)
     }
+
+    override fun setAdapter(adapter: Adapter<*>?) {
+        super.setAdapter(adapter)
+        (adapter as? SectionsAdapter)?.let { initView(it) }
+        logW("Using an Adapter that is not a SectionsAdapter the model will not be notified of value changes.")
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        subscriptions.clear()
+    }
+
+    fun showFormError(show: Boolean, smoothScrollToFirstError: Boolean = false) {
+        (adapter as? SectionsAdapter)?.let {
+            it.showErrors(show)
+            if (smoothScrollToFirstError) smoothScrollToPosition(it.firstErrorPosition())
+        }
+    }
+
+    private fun initModel() {
+
+        // Observe paths from model (FieldPath) and update the View
+        formModel.observeChanges()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .filter { it.pageIndex == pageIndex }
+                .map { it to formModel.pages[pageIndex].buildPageViewModel(formModel.storage).sections[it.sectionIndex] }
+                .subscribe(
+                        { updateViewIfSet(it.first, it.second) }, // TODO: use deconstruction
+                        { logE(it.message) })
+                .addTo(subscriptions)
+
+        // Observe model loading state if we want to be notified about errors
+        if (errorVisualization) {
+            formModel.observeFormState()
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(
+                            {
+                                when (it) {
+                                    READY -> formLoadingErrorSnackbar.dismiss()
+                                    LOADING -> formLoadingErrorSnackbar.dismiss()
+                                    ERROR -> formLoadingErrorSnackbar.show()
+                                    null -> formLoadingErrorSnackbar.show()
+                                }
+                            },
+                            { logE(it) }
+                    )
+                    .addTo(subscriptions)
+        }
+
+        // Start dynamic loading
+        formModel.loadDynamicValues()
+    }
+
+    private fun initView(sectionsAdapter: SectionsAdapter) {
+        // Observe values changes (FieldPathValue) and notify new values to the model
+        sectionsAdapter.observeValueChanges()
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .observeOn(Schedulers.io())
+                .retry()
+                .subscribe(
+                        { notifyModel(it.first, it.second) }, // TODO: use deconstruction
+                        { logE(it.message) })
+                .addTo(subscriptions)
+    }
+
+    private fun notifyModel(fieldPathSection: FieldPathSection, value: FieldValue) {
+        formModel.notifyValueChanged(FieldPath(fieldPathSection.fieldIndex, fieldPathSection.sectionIndex, pageIndex), value)
+    }
+
+    private fun updateViewIfSet(fieldPath: FieldPath, sectionViewModel: SectionViewModel) {
+        (adapter as? SectionsAdapter)?.updateField(fieldPath, sectionViewModel)
+    }
+
 }
--- src/main/kotlin/it/facile/form/ui/UiInterfaces.kt
+++ src/main/kotlin/it/facile/form/ui/UiInterfaces.kt
@@ -1,16 +1,10 @@
 package it.facile.form.ui
 
-import android.support.design.widget.Snackbar
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import it.facile.form.storage.FieldValue
-import it.facile.form.ui.adapters.FieldsLayouts
-import it.facile.form.ui.adapters.SectionsAdapter
-import it.facile.form.ui.viewmodel.FieldPath
 import it.facile.form.ui.viewmodel.FieldViewModel
 import it.facile.form.ui.viewmodel.FieldViewModelStyle.*
-import it.facile.form.ui.viewmodel.PageViewModel
-import rx.Observable
 
 /** Represent a Field that can show an error state */
 interface CanShowError {
@@ -70,66 +64,4 @@ interface ViewHolderFactory {
 
 interface CanBeDisabled {
     fun alpha(disabled: Boolean) = if (disabled) 0.4f else 1f
-}
-
-/** Interface representing the base functionality to be provided by a view containing a Form */
-interface FormView : it.facile.form.ui.View {
-    fun init(pageViewModels: List<PageViewModel>)
-    fun updateField(path: FieldPath,
-                    pageViewModel: PageViewModel)
-
-    fun observeValueChanges(): Observable<FieldPathWithValue>
-    fun scrollToFirstError()
-    fun showErrors(show: Boolean)
-    fun showLoadingErrors(show: Boolean)
-}
-
-
-/**
- * Convenient interface that represents a view containing a single page of a form.
- * It has default methods that execute common operations to build and sync the view and provides
- * methods provide custom behaviour.
- * */
-interface PageFormView : FormView {
-    var sectionsAdapter: SectionsAdapter?
-
-    override fun init(pageViewModels: List<PageViewModel>) {
-        if (sectionsAdapter == null) {
-            sectionsAdapter = SectionsAdapter(
-                    sectionViewModels = pageViewModels[0].sections,
-                    fieldsLayouts = getFieldsLayouts(),
-                    customBehaviours = getCustomBehaviours(),
-                    customPickerActions = getCustomPickerActions())
-            getRecyclerView().adapter = sectionsAdapter
-        }
-    }
-
-    override fun updateField(path: FieldPath, pageViewModel: PageViewModel) {
-        sectionsAdapter?.updateField(path, pageViewModel.sections[path.sectionIndex])
-    }
-
-    override fun observeValueChanges(): Observable<FieldPathWithValue> {
-        return sectionsAdapter?.observeValueChanges()?.map { FieldPath(it.first.fieldIndex, it.first.sectionIndex, 0) pathTo it.second } ?: Observable.empty()
-    }
-
-    override fun scrollToFirstError() {
-        val position = sectionsAdapter?.firstErrorPosition() ?: 0
-        if (position > -1) getRecyclerView().smoothScrollToPosition(position)
-    }
-
-    override fun showErrors(show: Boolean) {
-        sectionsAdapter?.showErrors(show)
-        if ((sectionsAdapter?.hasErrors() ?: false) and show)
-            Snackbar.make(getRecyclerView(),
-                    sectionsAdapter?.firstErrorMessage() ?: "Si è verificato un errore",
-                    Snackbar.LENGTH_SHORT)
-    }
-
-    fun getRecyclerView(): RecyclerView
-
-    fun getFieldsLayouts(): FieldsLayouts = FieldsLayouts()
-
-    fun getCustomPickerActions(): Map<String, ((FieldValue) -> Unit) -> Unit> = emptyMap()
-
-    fun getCustomBehaviours(): Map<String, () -> Unit> = emptyMap()
 }
