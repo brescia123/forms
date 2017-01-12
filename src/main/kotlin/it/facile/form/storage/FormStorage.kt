2925b7ab4ea7d48c8ebbcb4f980b5464581cf53d
Claudio Paccone <claudio.paccone@facile.it>
2017-01-12 11:41:48 +0100

    Change "executeAction" to "executeActions"

--- src/main/kotlin/it/facile/form/storage/FormStorage.kt
+++ src/main/kotlin/it/facile/form/storage/FormStorage.kt
@@ -23,77 +23,77 @@ class FormStorage(defaultEntries: Map<String, Entry>) : FormStorageApi {
 
     override fun getPossibleValues(key: String): FieldPossibleValues? = possibleValuesMap[key]
 
-    override fun ping(key: String, executeAction: Boolean) {
-        notify(key, executeAction)
+    override fun ping(key: String, executeActions: Boolean) {
+        notify(key, executeActions)
     }
 
     /* ---------- Writing methods ---------- */
 
-    override fun putValue(key: String, value: FieldValue, executeAction: Boolean) {
+    override fun putValue(key: String, value: FieldValue, executeActions: Boolean) {
         if (value == getValue(key)) return // No changes
         values.put(key, Entry(value, isHidden(key), isDisabled(key)))
-        notify(key, executeAction)
+        notify(key, executeActions)
     }
 
-    override fun disable(key: String, executeAction: Boolean) {
+    override fun disable(key: String, executeActions: Boolean) {
         values.put(key, Entry(getValue(key), isHidden(key), true))
-        notify(key, executeAction)
+        notify(key, executeActions)
     }
 
-    override fun enable(key: String, executeAction: Boolean) {
+    override fun enable(key: String, executeActions: Boolean) {
         values.put(key, Entry(getValue(key), isHidden(key), false))
-        notify(key, executeAction)
+        notify(key, executeActions)
     }
 
-    override fun clearValue(key: String, executeAction: Boolean) {
+    override fun clearValue(key: String, executeActions: Boolean) {
         values.put(key, Entry(Missing, isHidden(key), isDisabled(key)))
-        notify(key, executeAction)
+        notify(key, executeActions)
     }
 
-    override fun setVisibility(key: String, hidden: Boolean, executeAction: Boolean) {
+    override fun setVisibility(key: String, hidden: Boolean, executeActions: Boolean) {
         if (isHidden(key) == hidden) return // No changes
         values.put(key, Entry(getValue(key), hidden, isDisabled(key)))
-        notify(key, executeAction)
+        notify(key, executeActions)
     }
 
-    override fun putPossibleValues(key: String, possibleValues: FieldPossibleValues, executeAction: Boolean) {
+    override fun putPossibleValues(key: String, possibleValues: FieldPossibleValues, executeActions: Boolean) {
         if (possibleValues == getPossibleValues(key)) return // No changes
         possibleValuesMap.put(key, possibleValues)
-        clearValue(key, executeAction)
+        clearValue(key, executeActions)
     }
 
-    override fun switchPossibleValues(key: String, possibleValues: FieldPossibleValues, executeAction: Boolean) {
+    override fun switchPossibleValues(key: String, possibleValues: FieldPossibleValues, executeActions: Boolean) {
         val oldPossibleValues = getPossibleValues(key)
         if (possibleValues == oldPossibleValues) return // No changes
         possibleValuesMap.put(key, possibleValues)
         val selectedValue = values[key]?.value
         if (possibleValues.isCompatibleWith(oldPossibleValues) && selectedValue is Object) {
-            switchValueAtKey(key, selectedValue, executeAction)
+            switchValueAtKey(key, selectedValue, executeActions)
         } else {
-            clearValue(key, executeAction)
+            clearValue(key, executeActions)
         }
     }
 
-    override fun putValueAndSetVisibility(key: String, value: FieldValue, hidden: Boolean, executeAction: Boolean) {
+    override fun putValueAndSetVisibility(key: String, value: FieldValue, hidden: Boolean, executeActions: Boolean) {
         if (value == getValue(key) && hidden == isHidden(key)) return // No changes
         values.put(key, Entry(value, hidden, isDisabled(key)))
-        notify(key, executeAction)
+        notify(key, executeActions)
     }
 
-    override fun clearPossibleValues(key: String, executeAction: Boolean) {
+    override fun clearPossibleValues(key: String, executeActions: Boolean) {
         if (not(possibleValuesMap.containsKey(key))) return
         possibleValuesMap.remove(key)
-        clearValue(key, executeAction)
+        clearValue(key, executeActions)
     }
 
     override fun observe(): Observable<Pair<String, Boolean>> = publishSubject.asObservable()
 
-    private fun notify(key: String, executeAction: Boolean) = publishSubject.onNext(key to executeAction)
+    private fun notify(key: String, executeActions: Boolean) = publishSubject.onNext(key to executeActions)
 
-    private fun switchValueAtKey(fieldKey: String, value: Object, executeAction: Boolean) {
+    private fun switchValueAtKey(fieldKey: String, value: Object, executeActions: Boolean) {
         val fieldPossibleValues = possibleValuesMap[fieldKey]
         if (fieldPossibleValues is Available)
-            putValue(fieldKey, Object(fieldPossibleValues.list.first { it.key == value.value.key }), executeAction )
+            putValue(fieldKey, Object(fieldPossibleValues.list.first { it.key == value.value.key }), executeActions)
     }
 
     private fun FieldPossibleValues.isCompatibleWith(possibleValues: FieldPossibleValues?) =
