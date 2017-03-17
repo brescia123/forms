c451fa8eaf14ab09adcf4c6a4edea9c6be9c5b64
Claudio Paccone <claudio.paccone@facile.it>
2017-03-17 14:54:45 +0100

    Add MainActivity for test purpose

---
+++ src/androidTest/kotlin/it.facile.form/ui/MainActivityTest.kt
@@ -0,0 +1,53 @@
+package it.facile.form.ui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.CheckBox
+import it.facile.form.R
+import it.facile.form.model.models.FormModel
+import it.facile.form.storage.FormStorage
+import it.facile.form.ui.adapters.FieldsLayouts
+import it.facile.form.ui.adapters.SectionsAdapter
+import it.facile.form.ui.viewmodel.SectionViewModel
+import kotlinx.android.synthetic.main.activity_main.*
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
+
+class MainActivityTest : AppCompatActivity() {
+    private val formRecyclerView: FormRecyclerView by lazy { singlePageFormRecyclerView }
+    private var sectionsAdapter: SectionsAdapter? = null
+
+
+    private val formModel = TestForm.FORM_MODEL(FormStorage.empty(), Schedulers.io(), AndroidSchedulers.mainThread())
+
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initForm(formModel, formModel.pages[0].buildPageViewModel(formModel.storage).sections, 0)
+        showErrorsCheckBox.setOnClickListener { formRecyclerView.showFormError((it as CheckBox).isChecked) }
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    fun initForm(formModel: FormModel, sectionViewModels: List<SectionViewModel>, pageIndex: Int) {
+        formRecyclerView.formModel = formModel
+        formRecyclerView.pageIndex = pageIndex
+        if (sectionsAdapter == null) {
+            sectionsAdapter = SectionsAdapter(
+                    sectionViewModels = sectionViewModels,
+                    fieldsLayouts = FieldsLayouts(
+                            customBehaviours = R.layout.form_field_custom_behaviours,
+                            text = R.layout.form_field_text,
+                            toggle = R.layout.form_field_toggle,
+                            checkBox = R.layout.form_field_checkbox,
+                            inputText = R.layout.form_field_input_text,
+                            exceptionText = R.layout.form_field_exception_text,
+                            loading = R.layout.form_field_loading,
+                            sectionHeaders = R.layout.form_section_header to null))
+            formRecyclerView.adapter = sectionsAdapter
+        }
+    }
+}
